802456d9d7576e17142210f6bfed5bc9166fd940
Alexander Plavin <alexander@plav.in>
2024-01-01 00:00:00 +0000

    actual functionality

--- src/DictArrays.jl
+++ src/DictArrays.jl
@@ -1,3 +1,78 @@
 module DictArrays
 
+using Dictionaries
+using Indexing: getindices
+using StructArrays
+using DataAPI: Cols
+
+export DictArray, Cols
+
+
+Base.@propagate_inbounds Base.getproperty(d::D, s::Symbol) where {D<:AbstractDictionary} = hasfield(D, s) ? getfield(d, s) : d[s]
+Base.@propagate_inbounds function Base.setproperty!(d::D, s::Symbol, x) where {D<:AbstractDictionary}
+    hasfield(D, s) && return setfield!(d, s, x)
+    d[s] = x
+    return x
+end
+
+
+struct DictArray
+    dct::Dictionary{Symbol, <:AbstractVector}
+end
+
+Base.length(da::DictArray) = length(first(Dictionary(da)))
+Base.getindex(da::DictArray, i) = map(a -> a[i], Dictionary(da))
+Base.first(da::DictArray) = map(first, Dictionary(da))
+
+Base.getproperty(da::DictArray, i::Symbol) = Dictionary(da)[i]
+function Base.getindex(da::DictArray, i::Cols{<:Tuple{Vararg{Symbol}}})
+    ckeys = i.cols
+    cvals = getindices(Dictionary(da), ckeys)
+    StructArray(NamedTuple{ckeys}(cvals))
+end
+Base.getindex(da::DictArray, i::Cols{<:Tuple{Tuple{Vararg{Symbol}}}}) = da[Cols(only(i.cols)...)]
+Base.getindex(da::DictArray, i::Cols{<:Tuple{AbstractVector{Symbol}}}) = DictArray(getindices(Dictionary(da), Indices(only(i.cols))))
+Base.getindex(da::DictArray, i::Cols) = error("Not supported")
+
+Dictionary(da::DictArray) = getfield(da, :dct)
+Base.collect(da::DictArray) = map(i -> da[i], 1:length(da))
+
+function Base.map(f, da::DictArray)
+    t = tracedkeys(first(da))
+    fres = f(t)
+    _map(f, da, Cols(_accessed(t)...))
+end
+    
+function _map(f, da::DictArray, cols::Cols)
+    try
+        map(da[cols]) do r
+            try
+                f(r)
+            catch e
+                e isa ErrorException || rethrow()
+                m = match(r"has no field (\w+)$", e.msg)
+                isnothing(m) && rethrow()
+                throw(_map(f, da, Cols(cols.cols..., Symbol(m.captures[1]))))
+            end
+        end
+    catch e
+        e isa Exception && rethrow()
+        return e
+    end
+end
+
+
+struct TracedKeys
+    dct::Dictionary{Symbol, <:Any}
+    accessed::Vector{Symbol}
+end
+
+tracedkeys(dct) = TracedKeys(dct, [])
+_dct(t::TracedKeys) = getfield(t, :dct)
+_accessed(t::TracedKeys) = getfield(t, :accessed)
+
+Base.getproperty(t::TracedKeys, i::Symbol) = t[i]
+Base.getindex(t::TracedKeys, i::Symbol) = (push!(_accessed(t), i); _dct(t)[i])
+Base.getindex(t::TracedKeys, i::Tuple{Vararg{Symbol}}) = (append!(_accessed(t), i); getindices(_dct(t), i))
+
 end
--- test/runtests.jl
+++ test/runtests.jl
@@ -3,6 +3,59 @@ using TestItemRunner
 @run_package_tests
 
 
+@testitem "basic" begin
+    using Dictionaries
+    using FlexiMaps
+    using StructArrays
+
+    Nrow = 10^2
+    Ncol = 10^3
+    coldict = dictionary(flatmap(1:Ncol) do ci
+        [
+            Symbol(:a, ci) => ci .* (1:Nrow),
+            Symbol(:b, ci) => ["str_$i" for i in ci .* (1:Nrow)],
+            Symbol(:c, ci) => collect(ci .* (1:Nrow)),
+        ]	
+    end)
+    da = DictArray(coldict)
+    @test Dictionary(da) === coldict
+
+    @test da.a1 === 1 .* (1:Nrow)
+    
+    da1 = da[Cols([:a1, :b10, :c30])]
+    @test da1 isa DictArray
+    @test da1[12] == dictionary([:a1=>12, :b10=>"str_$(10*12)", :c30=>30*12])
+    @test da1.a1 === da.a1
+
+    sa1 = da[Cols((:a1, :b10, :c30))]
+    @test sa1 isa StructArray
+    @test isconcretetype(eltype(sa1))
+    @test sa1[12] === (a1=12, b10="str_$(10*12)", c30=30*12)
+    @test sa1.a1 == da.a1
+
+    @test da[12] isa Dictionary
+    @test da[12].a10 == 10*12
+    @test da[12].b40 == "str_$(40*12)"
+    @test first(da).c20 == 20
+
+    a = map(r -> r.a10, da)
+    @test a isa Vector{Int}
+    @test a[12] == 10*12
+    a = map(r -> r.a10 > 10 ? r.b3 : r.b38, da)
+    @test a isa Vector{String}
+    @test a[12] == "str_$(3*12)"
+
+    @test first(map(r -> r[:a1] * r[:c9], da)::Vector{Int}) == 9
+    
+    sa = map(r -> r[(:a1, :b10, :c30)], da)
+    @test sa isa StructArray
+    @test isconcretetype(eltype(sa))
+    @test sa[12] === (a1=12, b10="str_$(10*12)", c30=30*12)
+    @test sa.a1 == da.a1
+    @test sa.a1 !== da.a1
+end
+
+
 @testitem "_" begin
     import Aqua
     Aqua.test_all(DictArrays; ambiguities=false)
